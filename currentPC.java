55cef5847a15e0010d41730ab836316054523b38
chocopacotaco <50025972+chocopacotaco@users.noreply.github.com>
2019-11-01 14:18:14 -0400

    Add files via upload

--- currentPC.java
+++ currentPC.java
@@ -1,18 +1,33 @@
 public class currentPC{
+    int saveID;
+    String saveName;
     cpuPart currentCpu;
     gpuPart currentGpu;
     boardPart currentBoard;
     memoryPart currentMemory;
     powerPart currentPower;
     drivePart currentDrive;
-    public currentPC(cpuPart newCpu, gpuPart newGpu, boardPart newBoard, 
-    memoryPart newMemory, powerPart newPower, drivePart newDrive){
+    public currentPC(int saveCheck, String newName, cpuPart newCpu, gpuPart newGpu,
+    boardPart newBoard,memoryPart newMemory, powerPart newPower, drivePart newDrive){ 
+        saveID = saveCheck;
+        saveName = newName;
         currentCpu = newCpu;
         currentGpu = newGpu;
         currentBoard = newBoard;
         currentMemory = newMemory;
         currentPower = newPower;
         currentDrive = newDrive;
+    }
+    
+    public currentPC(){
+        saveID = 0;// zero indicates a new file
+        saveName = "untitled";
+        currentCpu = null;
+        currentGpu = null;
+        currentBoard = null;
+        currentMemory = null;
+        currentPower = null;
+        currentDrive = null;
     }
     
     public cpuPart getCpu(){
--- partBuilder.java
+++ partBuilder.java
@@ -9,9 +9,75 @@ public class partBuilder{
     ArrayList<memoryPart> memories = new ArrayList<memoryPart>();
     ArrayList<powerPart> powers = new ArrayList<powerPart>();
     ArrayList<drivePart> drives = new ArrayList<drivePart>();
+    ArrayList<cpuPart> pcs = new ArrayList<cpuPart>();
     public partBuilder(fileReader newInfo){
         buildCPUs();
+        buildGPUs();
+        buildPCs();
     }
+    
+    public void buildPCs(){
+        try{
+            FileReader out = new FileReader("saves.csv");
+            BufferedReader out2 = new BufferedReader(out);
+            CSVReader reader = new CSVReader(out2);
+            List<String[]> records = reader.readAll();
+            for (int i = 0; i < records.size(); i++) {
+                String[] record = records.get(i);
+                if(i != 0){
+                    int id = Integer.parseInt(record[0]);
+                    String saveName = record[1];
+                    int cpufinder = Integer.parseInt(record[2]);
+                    cpuPart savedCpu = null;
+                    for(int j = 0; j < cpus.size(); j++){
+                        if(cpus.get(j).cpuID == cpufinder){
+                            savedCpu= cpus.get(j);
+                        }
+                    }
+                    int gpufinder = Integer.parseInt(record[3]);
+                    gpuPart savedGpu = null;
+                    for(int j = 0; j < gpus.size(); j++){
+                        if(gpus.get(j).gpuID == gpufinder){
+                            savedGpu= gpus.get(j);
+                        }
+                    }
+                    int boardfinder = Integer.parseInt(record[4]);
+                    boardPart savedBoard = null;
+                    for(int j = 0; j < boards.size(); j++){
+                        if(boards.get(j).boardID == boardfinder){
+                            savedBoard= boards.get(j);
+                        }
+                    }
+                    int memoriesfinder = Integer.parseInt(record[5]);
+                    memoryPart savedMemories = null;
+                    for(int j = 0; j < memories.size(); j++){
+                        if(memories.get(j).memoryID == memoriesfinder){
+                            savedMemories= memories.get(j);
+                        }
+                    }
+                    int powersfinder = Integer.parseInt(record[6]);
+                    powerPart savedPower = null;
+                    for(int j = 0; j < powers.size(); j++){
+                        if(powers.get(j).powerID == powersfinder){
+                            savedPower = powers.get(j);
+                        }
+                    }
+                    int drivefinder = Integer.parseInt(record[7]);
+                    drivePart savedDrive = null;
+                    for(int j = 0; j < powers.size(); j++){
+                        if(drives.get(j).storageID == drivefinder){
+                            savedDrive = drives.get(j);
+                        }
+                    }
+                    currentPC savedPc = new currentPC(id,saveName,savedCpu,savedGpu,
+                    savedBoard,savedMemories,savedPower,savedDrive);
+                }
+            }
+        } catch(Exception e) {
+        }
+    }
+    
+    
     
     public void buildCPUs(){
         try{
